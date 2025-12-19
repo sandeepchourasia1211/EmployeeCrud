84ec0ff7e3d3cf9c4641291bd01c1baff904f486
Sandeep Chourasia <schaurasiya1211@gmail.com>
2025-12-19 16:55:39 +0530

    [Sandeep - Service layer designed]

---
+++ EmployeeManagement/EmployeeManagement/Services/EmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeManagement.Model;
+using EmployeeManagement.Repositories;
+
+namespace EmployeeManagement.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeRepository _repo;
+
+        public EmployeeService(IEmployeeRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<Employee> GetAll() => _repo.GetAll();
+        public Employee GetById(int id) => _repo.GetById(id);
+        public void Add(Employee emp) => _repo.Add(emp);
+        public void Update(int id, Employee emp) => _repo.Update(id, emp);
+        public void Delete(int id) => _repo.Delete(id);
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.Model;
+
+namespace EmployeeManagement.Services
+{
+    public interface IEmployeeService
+    {
+        public List<Employee> GetAll();
+        public Employee GetById(int id);
+        public void Add(Employee emp);
+        public void Update(int id, Employee emp);
+        public void Delete(int id);
+    }
+}
